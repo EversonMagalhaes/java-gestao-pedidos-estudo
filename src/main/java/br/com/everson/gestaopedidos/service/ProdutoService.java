1b6a6caa0e333596e1b802ce4bfb3f53c2ecbc87
EversonMagalhaes <everslay@gmail.com>
2026-01-11 23:12:12 -0300

    Atualização para o metodo @Transactional em criar e atualizar Produtos

--- src/main/java/br/com/everson/gestaopedidos/service/ProdutoService.java
+++ src/main/java/br/com/everson/gestaopedidos/service/ProdutoService.java
@@ -2,6 +2,7 @@ package br.com.everson.gestaopedidos.service;
 
 import br.com.everson.gestaopedidos.domain.Produto;
 import br.com.everson.gestaopedidos.repository.ProdutoRepository;
+
 import org.springframework.stereotype.Service;
 import br.com.everson.gestaopedidos.dto.ProdutoDTO;
 
@@ -10,6 +11,7 @@ import java.util.List;
 import br.com.everson.gestaopedidos.dto.ProdutoCreateDTO;
 
 import br.com.everson.gestaopedidos.exception.ProdutoNaoEncontradoException;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 
@@ -34,7 +36,7 @@ public class ProdutoService {
                 )
                 .toList();
     }
-
+    @Transactional
     public void criar(ProdutoCreateDTO dto) {
         Produto produto = new Produto(
                 dto.getNome(),
@@ -43,8 +45,11 @@ public class ProdutoService {
         );
 
         produtoRepository.save(produto);
+        // No 'criar', o .save() ainda é comum para deixar claro,
+        // mas o @Transactional garante que se algo der errado após essa linha,
+        // ele desfaz (rollback) a criação
     }
-
+    @Transactional
     public void atualizar(Long id, ProdutoCreateDTO dto) {
         Produto produto = this.buscarPorId(id); // Acha o cara
 
@@ -52,7 +57,7 @@ public class ProdutoService {
         produto.setPreco(dto.getPreco()); // Troca o preço
         produto.setAtivo(dto.getAtivo()); // Troca o status
 
-        produtoRepository.save(produto);  // Salva no Postgres
+       // produtoRepository.save(produto); // <--- PODE COMENTAR!
     }
 
     public Produto buscarPorId(Long id) {
